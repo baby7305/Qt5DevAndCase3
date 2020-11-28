cab94a783a0713077f57eb4cc7af37e080cb8b8a
baby7305 <songhaibei7305@163.com>
2020-11-28 09:56:56 +0800

    修改xmake文件

--- Dialog/xmake.lua
+++ Dialog/xmake.lua
@@ -4,7 +4,6 @@ target("Dialog")
     add_rules("qt.widgetapp")
     add_headerfiles("*.h")
     add_files("*.cpp")
-    add_files("dialog.ui")
     -- add files with Q_OBJECT meta (only for qt.moc)
     add_files("dialog.h")
 
